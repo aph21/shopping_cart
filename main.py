57de4c685b3a70c31709b00d9b1abe74cafcbbba
Anjanaph21 <anjanahegdee21@gmail.com>
2026-03-20 17:38:17 +0530

    Implement basic shopping cart functionality with item addition

---
+++ main.py
@@ -0,0 +1,13 @@
+cart = []
+
+def add_item(name, price, qnt):
+    cart.append({"name": name, "price": price, "quantity": qnt})
+
+#main menu 
+name =  input("Item name: ")
+price = float(input("Price: "))
+qnt = int(input("Quantity: "))
+
+add_item(name, price, qnt)
+
+print(cart)
